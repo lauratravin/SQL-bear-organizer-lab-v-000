b86953ce0be9fdea40e743f4ce37f15b3f2c919b
Laura Messa Travin <laura.travin@gmail.com>
2018-09-07 23:54:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,11 +36,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color)
+  "SELECT color, COUNT(color)
    FROM bears
-   WHERE color = 'Black'"
+   WHERE color = 'Dark Brown'"
 end
-# 
+#
 # def counts_number_of_bears_with_goofy_temperaments
 #   "SELECT name,
 #           age
