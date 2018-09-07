d2dae56bb458970b2c50c878991003113ca2eaca
Laura Messa Travin <laura.travin@gmail.com>
2018-09-07 23:03:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@ end
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT name
    FROM bears
-   ASC"
+   ORDER BY NAME ASC"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
