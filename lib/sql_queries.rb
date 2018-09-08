05cd1195ea9708c8cfa102215cd0504403a49999
Laura Messa Travin <laura.travin@gmail.com>
2018-09-08 00:01:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,12 +42,11 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT name,
-          age
+  "SELECT COUNT(temperament)        
    FROM bears
-   ORDER BY age ASC
-   LIMIT 1"
+   WHERE temperament = 'googfy'"
 end
+
 # def selects_bear_that_killed_Tim
 #   "SELECT name,
 #           age
